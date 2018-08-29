31e03f1041fc81224b5a928eed320422c930e542
athena15 <brenner.heintz@gmail.com>
2018-08-29 11:10:52 -0700

    Added link to kata.

--- delete_occurrences.py
+++ delete_occurrences.py
@@ -1,6 +1,5 @@
-
 # Codewars 6 kyu challenge - 'Delete occurrences of an element if it occurs more than n times'.
-# Delete occurrences of an element if it occurs more than n times
+# https://www.codewars.com/kata/554ca54ffa7d91b236000023
 
 
 from collections import defaultdict
