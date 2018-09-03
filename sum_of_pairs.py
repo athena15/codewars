8db3ae8f148058400dc0339acfac5d3378604a24
athena15 <brenner.heintz@gmail.com>
2018-09-03 13:57:25 -0700

    Rewrote code to be more efficient with O(n) algorithm complexity.

--- sum_of_pairs.py
+++ sum_of_pairs.py
@@ -2,34 +2,16 @@
 # Given a list of integers and a single sum value, return the first two values in order of appearance that add up to form the sum.
 # https://www.codewars.com/kata/sum-of-pairs/train/python
 
-from itertools import combinations
-
 
 def sum_pairs(ints, s):
-	x = list(combinations(ints, 2))
-	y = [i for i in x if i[0] + i[1] == s]
-	if len(y) == 0:
-		return None
-
-	lowest = 9999
-	answer = []
-	for match in y:
-		if match[0] == match[1]:
-			match_two = [i for i, n in enumerate(ints) if n == match[0]][1]
-
-			if match_two < lowest:
-				lowest = match_two
-				answer = list(match)
-
-
+	seen_numbers = set()
+	needed_nums = set()
+	for num in ints:
+		if num in needed_nums:
+			return [s - num, num]
 		else:
-			match_two = [i for i, n in enumerate(ints) if n == match[1]][0]
+			needed_nums.add(s - num)
 
-			if match_two < lowest:
-				lowest = match_two
-				answer = list(match)
-
-	return answer
 
 
 sum_pairs([2, 13, 12, 4, 3, 6, 7, 3, 1, 1], 10)
