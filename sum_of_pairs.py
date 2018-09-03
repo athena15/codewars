d09584f4f0cc7df408a8d410b1ef310c322a5dab
athena15 <brenner.heintz@gmail.com>
2018-09-03 13:38:09 -0700

    Initial commit.

--- sum_of_pairs.py
+++ sum_of_pairs.py
@@ -1,3 +1,7 @@
+# CodeWars Sum of Pairs Solution <5 kyu>
+# Given a list of integers and a single sum value, return the first two values in order of appearance that add up to form the sum.
+# https://www.codewars.com/kata/sum-of-pairs/train/python
+
 from itertools import combinations
 
 
@@ -9,33 +13,27 @@ def sum_pairs(ints, s):
 
 	lowest = 9999
 	answer = []
-	print(y)
 	for match in y:
 		if match[0] == match[1]:
-			match_one = [i for i, n in enumerate(ints) if n == match[0]][0]
 			match_two = [i for i, n in enumerate(ints) if n == match[0]][1]
-			print(f'Matches are: {match_one}, {match_two}')
+
 			if match_two < lowest:
-				print(f'match_2 = {match_two}, is less than lowest = {lowest}')
 				lowest = match_two
 				answer = list(match)
-				print(f'Answer is now {answer}')
+
 
 		else:
-			match_one = [i for i, n in enumerate(ints) if n == match[0]][0]
 			match_two = [i for i, n in enumerate(ints) if n == match[1]][0]
-			print(f'Matches are: {match_one}, {match_two}')
+
 			if match_two < lowest:
-				print(f'match_2 = {match_two}, is less than lowest = {lowest}')
 				lowest = match_two
 				answer = list(match)
-				print(f'Answer is now {answer}')
 
-	print(answer)
+	return answer
 
 
-# sum_pairs([2, 13, 12, 4, 3, 6, 7, 3, 1, 1], 10)
+sum_pairs([2, 13, 12, 4, 3, 6, 7, 3, 1, 1], 10)
 
-# sum_pairs([10, 5, 2, 3, 7, 5], 10)
+sum_pairs([10, 5, 2, 3, 7, 5], 10)
 
 sum_pairs([1, 2, 3, 4, 1, 0], 2)
